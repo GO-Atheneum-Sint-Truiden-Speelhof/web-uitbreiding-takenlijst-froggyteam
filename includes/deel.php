c152b9b96bb4fba2c3265bfa1bd70b4090121442
CODEMaSteR69MATT <matthew.joossens@atheneumsinttruiden.be>
2024-11-25 14:07:39 +0100

    mo

--- includes/deel.php
+++ includes/deel.php
@@ -2,8 +2,7 @@
 <div class="jumbotron">
 			<h1 class="display-4">Klaar om deel te nemen? Shoot!</h1>
 		</div>
-<form method="post"  action="home.php?page=confirm" >
-    <!-- Right section -->
+<form method="post"  action="taken.php" >
     <div class="right-section">
         <label for="titel">titel van taak</label><br>
         <p><input required id="titel" type="text" name="titel" size="46"></p>
@@ -14,10 +13,29 @@
         <label for="beschrijving">beschrijf u taak</label><br>
 		<p><textarea required id="beschrijving" name="beschrijving" rows="5" cols="50"></textarea></p>
     
-	<input type="submit" value="deelnemen">
+	<input type="submit" value="taken">
 	</div>
 
     <div style="clear: both;"></div>
-    
+
+    <?php
+        $servername = "localhost";
+        $username = "root"; 
+        $password = ""; 
+        $dbname = "todo";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Verbinding mislukt: " . $conn->connect_error);
+        }
+
+        
+
+        
+        $conn->close();
+?>
+
     
 </form>
+
